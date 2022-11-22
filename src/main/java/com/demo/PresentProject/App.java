af1a8cb0700258e46180a60fa7add48558de414f
medhajbhandari <medhajbhandari@gmail.com>
2022-11-22 12:33:54 +0530

    xyz

---
+++ src/main/java/com/demo/PresentProject/App.java
@@ -0,0 +1,13 @@
+package com.demo.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
